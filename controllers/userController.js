19a62164501bcee3553fad9626517bab473eb430
estong jamion <estong.jamion@gmail.com>
2023-01-12 08:56:18 +0300

    typo error fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,10 +46,10 @@ const registerUser = asyncHandler(async (req, res) => {
   //send http-only cookie
   res.cookie("token", token, {
     path: "/",
-    HttpOnly: true,
+    httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
-    SameSite: "none",
-    Secure: true,
+    sameSite: "none",
+    secure: true,
   });
 
   if (user) {
@@ -95,10 +95,10 @@ const loginUser = asyncHandler(async (req, res) => {
   //send http-only cookie
   res.cookie("token", token, {
     path: "/",
-    HttpOnly: true,
+    httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
-    SameSite: "none",
-    Secure: true,
+    sameSite: "none",
+    secure: true,
   });
 
   if (user && passwordIsCorrect) {
@@ -122,7 +122,7 @@ const loginUser = asyncHandler(async (req, res) => {
 const logout = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
     path: "/",
-    HttpOnly: true,
+    httpOnly: true,
     expires: new Date(0), // expires right away
     sameSite: "none",
     secure: true,
